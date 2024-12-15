2b7e8ceb50b657f574e8e7c000c19781d38b8f1f
Alexander Zaburdaev <aleksandr.zaburdaev@inthemelab.com>
2024-12-15 13:58:37 +0300

    поправил баг с белым экраном

--- frontend-v1/src/components/card/project/ProjectCard.tsx
+++ frontend-v1/src/components/card/project/ProjectCard.tsx
@@ -6,7 +6,7 @@ import { Project } from 'store/slices/projects/projectsSlice';
 export const ProjectCard: React.FC<Project> = memo((project: Project) => {
     const navigate = useNavigate();
     return (
-        <div className="service-card-page" style={{ backgroundImage: `url(${project?.ProjectPhoto[0].url})`}}>
+        <div className="service-card-page" style={{ backgroundImage: `url(${project?.ProjectPhoto[0]?.url || ""})`}}>
             <div className="service-card-page_header">
                 <h2 className="service-card-page_title">{project.name}</h2>
             </div>
